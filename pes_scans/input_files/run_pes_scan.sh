831aab1696931d2cfccaca447d81fa1f1d182876
Ria <riajobanputra@gmail.com>
2025-06-08 18:54:25 +0100

    Add files via upload

---
+++ pes_scans/input_files/run_pes_scan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=SPE
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --mem=128g
+#SBATCH --time=72:00:00
+#SBATCH --partition=defq
+
+# Load modules
+module purge
+module load orca-uoneasy/5.0.4-gompi-2022a-UCX-1.15.0
+
+# Export paths
+export ORCA=`which orca`
+
+# Run ORCA
+${ORCA} *.inp > orca.out
